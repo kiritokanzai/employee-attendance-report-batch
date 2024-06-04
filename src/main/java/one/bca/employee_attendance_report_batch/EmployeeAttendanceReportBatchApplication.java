74f31bca22726e80d93e9135aed9015c6f466ddb
AUSTIN ANDIKA TANUJAYA <austin_tanujaya@bca.co.id>
2024-06-04 16:05:43 +0700

    Added @EnableBatchProcessing

--- src/main/java/one/bca/employee_attendance_report_batch/EmployeeAttendanceReportBatchApplication.java
+++ src/main/java/one/bca/employee_attendance_report_batch/EmployeeAttendanceReportBatchApplication.java
@@ -1,9 +1,11 @@
 package one.bca.employee_attendance_report_batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class EmployeeAttendanceReportBatchApplication {
 
 	public static void main(String[] args) {

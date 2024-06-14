ed5c0d8e0fa88cd879e7b605f336014eadfc03d8
DEAN ANANDA RAMADHAN <dean_ramadhan@bca.co.id>
2024-06-14 10:19:57 +0700

    add: job scheduler

--- src/main/java/one/bca/employee_attendance_report_batch/EmployeeAttendanceReportBatchApplication.java
+++ src/main/java/one/bca/employee_attendance_report_batch/EmployeeAttendanceReportBatchApplication.java
@@ -1,15 +1,16 @@
 package one.bca.employee_attendance_report_batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@EnableBatchProcessing
+@EnableScheduling
 public class EmployeeAttendanceReportBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeAttendanceReportBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeAttendanceReportBatchApplication.class, args);
+    }
 
 }
---
+++ src/main/java/one/bca/employee_attendance_report_batch/scheduler/JobScheduler.java
@@ -0,0 +1,30 @@
+package one.bca.employee_attendance_report_batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JobScheduler {
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled(cron = "0 0 1 * *") // Schedule at first day of the month
+    public void performBatchJob() throws Exception {
+        log.info("Running job {} on {}", job.getName(), LocalDateTime.now());
+        JobParameters params = new JobParametersBuilder()
+                .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+        jobLauncher.run(job, params);
+    }
+
+}

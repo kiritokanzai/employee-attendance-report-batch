be2cd3a8e8e3f32f4c1cf4c1217be3fced53a3e7
ADRIEL TRAVIS ABRAHAM <adriel_abraham@bca.co.id>
2024-06-14 14:45:15 +0700

    Added unit testing for job

--- src/test/java/one/bca/employee_attendance_report_batch/EmployeeAttendanceReportDtoBatchApplicationTests.java
+++ src/test/java/one/bca/employee_attendance_report_batch/EmployeeAttendanceReportDtoBatchApplicationTests.java
@@ -1,13 +1,52 @@
 package one.bca.employee_attendance_report_batch;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBatchTest
 class EmployeeAttendanceReportDtoBatchApplicationTests {
 
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Autowired
+	private JobRepositoryTestUtils jobRepositoryTestUtils;
+
 	@Test
-	void contextLoads() {
+	void testJobExecution() throws Exception {
+		// given
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("input.file", "filename")
+				.toJobParameters();
+
+		// random job parameters
+		// this.jobLauncherTestUtils.getUniqueJobParameterBuilder()
+
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+		// then
+		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 	}
+
+	@AfterEach
+	void setUp() {
+		this.jobRepositoryTestUtils.removeJobExecutions();
+	}
+
+	/*@Test
+	void contextLoads() {
+	}*/
 
 }

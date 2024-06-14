0aff1280aca17692c618d062962714cd3d9a0535
DEAN ANANDA RAMADHAN <dean_ramadhan@bca.co.id>
2024-06-14 14:43:38 +0700

    add: multithreading capability on first

--- src/main/java/one/bca/employee_attendance_report_batch/configuration/AttendanceDataStep.java
+++ src/main/java/one/bca/employee_attendance_report_batch/configuration/AttendanceDataStep.java
@@ -8,6 +8,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
@@ -17,11 +19,16 @@ public class AttendanceDataStep {
     private final DataSourceTransactionManager transactionManager;
     private final AttendanceReader reader;
 
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor("attendance-data-task");
+    }
+
     public Step getStep() {
         return new StepBuilder("attendanceDataStep", jobRepository)
                 .<Attendance, Attendance>chunk(50, transactionManager)
                 .reader(reader.itemReader())
                 .writer(new AttendanceDataJdbcWriter().itemWriter(transactionManager.getDataSource()))
+                .taskExecutor(taskExecutor())
                 .build();
     }
 }
